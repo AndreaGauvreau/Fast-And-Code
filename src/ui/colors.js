e878b8da504de4824b3dd7a4eae78ee1ba52db1c
Andrea.Gauvreau <andrea@teachizi.fr>
2023-04-26 10:46:34 +0200

    initalisation style

---
+++ src/ui/colors.js
@@ -0,0 +1,12 @@
+export const colorsUi = {
+  red1: '#FF6347',
+  red2: '#dc442e',
+  red3: '#8d0000',
+  green1: '#7FFF00',
+  green2: '#009900',
+  white1: '#F7F7F7',
+  white2: '#ededed',
+  grey1: '#c4c4c4',
+  grey2: '#333333',
+  grey3: '#5c5c5c',
+}
